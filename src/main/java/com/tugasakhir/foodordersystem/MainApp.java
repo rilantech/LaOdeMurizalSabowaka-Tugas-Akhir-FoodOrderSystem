beaadc943ad63645ded453141f3fe0569b2ff338
rilantech <laodemurizal7@gmail.com>
2025-09-12 12:16:39 +0800

    Membuat Setup Project Pertama

--- src/main/java/com/tugasakhir/foodordersystem/MainApp.java
+++ src/main/java/com/tugasakhir/foodordersystem/MainApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FoodordersystemApplication {
+public class MainApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FoodordersystemApplication.class, args);
+		SpringApplication.run(MainApp.class, args);
 	}
 
 }
