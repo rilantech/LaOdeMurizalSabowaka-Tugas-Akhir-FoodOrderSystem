d5cd92a401a124922d13b52a6f31fe5baee90499
rilantech <laodemurizal7@gmail.com>
2025-09-12 12:00:34 +0800

    membuat setup project

---
+++ src/main/java/com/tugasakhir/foodordersystem/FoodordersystemApplication.java
@@ -0,0 +1,13 @@
+package com.tugasakhir.foodordersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodordersystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodordersystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tugasakhir/foodordersystem/FoodordersystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tugasakhir.foodordersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodordersystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
