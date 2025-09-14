fa9956d2b132d6838fa9c053be3c30b5c5d7875c
rilantech <laodemurizal7@gmail.com>
2025-09-14 12:50:15 +0800

    Menambahkan anotasi @Tag di class AuthController.java untuk memberikan nama Auth API di swagger

--- src/main/java/com/tugasakhir/foodordersystem/controller/app/AuthController.java
+++ src/main/java/com/tugasakhir/foodordersystem/controller/app/AuthController.java
@@ -5,6 +5,7 @@ import com.tugasakhir.foodordersystem.model.request.LoginRequestRecord;
 import com.tugasakhir.foodordersystem.model.request.UserRequestRecord;
 import com.tugasakhir.foodordersystem.model.response.BaseResponse;
 import com.tugasakhir.foodordersystem.service.app.AuthService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("auth")
 @RequiredArgsConstructor
+@Tag(name = "Auth Api")
 public class AuthController {
 
     private final AuthService authService;
